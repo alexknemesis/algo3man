079eae734dc7a039da322baf81e4c0bd94e87bde
transparencia.tornasolada <transparencia.tornasolada@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-11-16 20:09:23 +0000

    Agregue Juego, Celda, Mapa y FabricaDeMapas. Le agregó los encabezados de los metodos despues vemos quien los completa. Ahora me enfoco en FabricaDeMapas

---
+++ trunk/tp3/src/modelo/Juego.java
@@ -0,0 +1,26 @@
+package modelo;
+
+import java.awt.Point;
+
+public class Juego {
+	
+	int nivel;
+	int puntaje;
+	int vida;
+	FabricaDeMapas fabrica;
+	Mapa mapa;
+	
+	public void avanzarNivel(){
+		
+	}
+	
+	public void gameOver(){
+		
+	}
+	
+	public Celda getCelda(Point punto){
+		return null;
+		
+	}
+
+}
