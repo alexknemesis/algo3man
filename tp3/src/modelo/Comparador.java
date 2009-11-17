4d796f13ece7b9c06f95cc52fd4f0939e849fbc4
transparencia.tornasolada <transparencia.tornasolada@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-11-17 17:45:27 +0000

    Arreglé el comparador porque no funcionaba bien.

--- tp3/src/modelo/Comparador.java
+++ tp3/src/modelo/Comparador.java
@@ -9,7 +9,7 @@ public class Comparador implements Comparator{
 
 	public int compare(Object arg0, Object arg1) {
 		
-		long norma1,norma2;
+		double norma1,norma2;
 		
 		if (!(arg0 instanceof Point)|| !(arg1 instanceof Point)){
 			throw new ClassCastException();
@@ -18,11 +18,16 @@ public class Comparador implements Comparator{
 		Point clave1 = (Point) arg0;
 		Point clave2 = (Point) arg1;
 		
-		norma1=Math.round(Math.sqrt(Math.pow(clave1.x,2)+ Math.pow(clave1.y,2)));
-		norma2=Math.round(Math.sqrt(Math.pow(clave2.x,2)+ Math.pow(clave2.y,2)));
+		norma1=Math.sqrt(Math.pow(clave1.x,2)+ Math.pow(clave1.y,2));
+		norma2=Math.sqrt(Math.pow(clave2.x,2)+ Math.pow(clave2.y,2));
 		
-		
-		if (norma1==norma2 ){
+		if((clave1.x ==clave2.y)&&(clave1.y==clave2.x)&&(clave1.x>clave2.x)){
+			return 1;
+		 
+		}else if((clave1.x ==clave2.y)&&(clave1.y==clave2.x)&&(clave1.x<clave2.x)){
+			return -1;
+		 
+		}else if (norma1==norma2){
 			return 0;
 		}else if(norma1<norma2){
 			return -1;
