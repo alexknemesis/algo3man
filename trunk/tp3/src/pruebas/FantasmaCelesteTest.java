eca33219e53dc6776c3432fd5afb2334011c6019
cripton <cripton@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-11-17 01:18:36 +0000

    agregada clase de pruebas del FantasmaCeleste para ir extremeprogramando un poco ;)

---
+++ trunk/tp3/src/pruebas/FantasmaCelesteTest.java
@@ -0,0 +1,27 @@
+package pruebas;
+
+import junit.framework.TestCase;
+
+public class FantasmaCelesteTest extends TestCase {
+
+	public FantasmaCelesteTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testVivir() {
+		fail("Not yet implemented");
+	}
+
+	public void testFantasmaCeleste() {
+		fail("Not yet implemented");
+	}
+
+}
