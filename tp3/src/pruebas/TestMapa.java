0f06c6a47908af883ef13151105a6c0f9e654974
transparencia.tornasolada <transparencia.tornasolada@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-11-17 15:59:42 +0000

    Hice el testMapa y el mapa. Al final use un TreeMap con un comparator Comparador para que compare datos de tipo Point. No probe el testMapa porque no está creado Juego ni las celdas. Una vez que esten hechas se puede empezar a probar.

--- tp3/src/pruebas/TestMapa.java
+++ tp3/src/pruebas/TestMapa.java
@@ -7,6 +7,7 @@ import modelo.CeldaPared;
 import modelo.Juego;
 import modelo.Mapa;
 import modelo.MapaConstruirException;
+import modelo.RangoException;
 import modelo.RestarPuntoException;
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -20,7 +21,11 @@ public class TestMapa extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 		this.juego = new Juego();
-		this.mapa = new Mapa(2,2,this.juego);
+		this.mapa = new Mapa(2,4,this.juego);
+		
+		Point punto = new Point(1,1);
+		Celda celda = new CeldaPared();
+		this.mapa.agregar(punto, celda);
 	}
 
 	public void testMapa_Fail() {
@@ -66,10 +71,28 @@ public class TestMapa extends TestCase {
 
 	public void testAgregar_Ok() {
 		Celda celda = new CeldaPared();
-		Point punto = new Point(1,1);
-		this.mapa.agregar(punto, celda);
-		Point celda2 = this.mapa.getCelda(punto);
-		Assert.assertEquals(celda, celda2);
+		Point punto = new Point(2,2);
+		try{
+			this.mapa.agregar(punto, celda);
+			
+			Assert.assertEquals(true, true);
+		}catch(RangoException e){
+			fail("No Debería mandar excepción pero no lo hizo");
+		}
+		
+	}
+	
+	public void testAgregar_Fail() {
+		Celda celda = new CeldaPared();
+		Point punto = new Point(4,4);
+		try{
+			this.mapa.agregar(punto, celda);
+			fail("Debería mandar excepción pero no lo hizo");
+			
+		}catch(RangoException e){
+			
+			Assert.assertEquals(true, true);
+		}
 		
 	}
 	
@@ -81,6 +104,33 @@ public class TestMapa extends TestCase {
 	public void testGetColumnas(){
 		Assert.assertEquals(this.mapa.getColumnas(), 2);
 		
+	}
+	
+	public void testgetCelda_Ok(){
+		Point punto = new Point(1,1);
+		
+		try{
+			this.mapa.getCelda(punto);
+			Assert.assertEquals(true, true);
+			
+		}catch (RangoException e){
+			fail("No debería mandar excepción pero no lo hizo");
+			
+		}
+	}
+	
+	public void testgetCelda_Fail(){
+		Point punto = new Point(5,5);
+		
+		try{
+			this.mapa.getCelda(punto);
+			fail("No debería mandar excepción pero no lo hizo");
+			
+			
+		}catch (RangoException e){
+			Assert.assertEquals(true, true);
+			
+		}
 	}
 
 }
