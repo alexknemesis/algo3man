2d5220564c83f0a7380d16eb812b7288f7995e4e
cripton <cripton@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-11-24 18:47:18 +0000

    Implementados los métodos direccionParaMaximaDistancia y direccionParaMinimaDistancia

--- tp3/src/grupo9/algo3man/modelo/Fantasma.java
+++ tp3/src/grupo9/algo3man/modelo/Fantasma.java
@@ -42,7 +42,6 @@ public abstract class Fantasma extends Personaje {
 	
 	public void dispersarse(){
 		this.estado = DISPERSO;
-		this.objetivoActual= celdaPreferida;
 	}
 	
 	public int getEstado(){
@@ -54,13 +53,86 @@ public abstract class Fantasma extends Personaje {
 	}
 	
 	protected int direccionParaMinimaDistanciaA(Objetivo objetivo){
-		//TODO
-		return 0;
-	}
+		int direccionAMinimaDistancia;
+		double distanciaMinima;
+		Celda [] celdasAlrededor;
+			
+		//Inicializo la variable con la máxima distancia que puede existir en el juego
+		distanciaMinima = juego.getDimensiones().distance(new Point(0,0));
 
+		direccionAMinimaDistancia = -1;
+		
+		celdasAlrededor = new Celda[4];
+		
+		celdasAlrededor[ARRIBA] = juego.getCelda(this.posicion.x,this.posicion.y - 1);
+		celdasAlrededor[ABAJO] = juego.getCelda(this.posicion.x,this.posicion.y + 1);
+		celdasAlrededor[IZQUIERDA] = juego.getCelda(this.posicion.x,this.posicion.y - 1);
+		celdasAlrededor[DERECHA] = juego.getCelda(this.posicion.x,this.posicion.y + 1);
+		
+		//Descarto moverme hacia atrás
+		celdasAlrededor[direccionContraria(this.direccion)]= null;
+		
+		for (int i = 0; i<4; i++){
+			if(celdasAlrededor[i] != null)
+				if(celdasAlrededor[i].esTransitable())
+					if (objetivo.getDistancia(celdasAlrededor[i].getPosicion()) < distanciaMinima){
+						distanciaMinima =objetivo.getDistancia(celdasAlrededor[i].getPosicion());
+						direccionAMinimaDistancia = i;
+					}
+		}
+		
+		if (direccionAMinimaDistancia != -1){
+			return direccionAMinimaDistancia; //Encontró una dirección nueva
+		}
+		return direccionContraria(this.direccion); //Encontró una pared y debe dar vuelta
+	}
+	
+	private int direccionContraria(int direccion){
+		int irA=0;
+		
+		switch (this.direccion){
+			case ARRIBA: irA = ABAJO;break;
+			case ABAJO: irA = ARRIBA;break;
+			case IZQUIERDA: irA = DERECHA;break;
+			case DERECHA: irA = IZQUIERDA;break;
+		}
+		return irA;
+		
+	}
+	
 	protected int direccionParaMaximaDistanciaA(Objetivo objetivo){
-		//TODO
-		return 0;
+		int direccionAMaximaDistancia;
+		double distanciaMaxima;
+		Celda [] celdasAlrededor;
+			
+		//Inicializo la variable con la máxima distancia que puede existir en el juego
+		distanciaMaxima = juego.getDimensiones().distance(new Point(0,0));
+
+		direccionAMaximaDistancia = -1;
+		
+		celdasAlrededor = new Celda[4];
+		
+		celdasAlrededor[ARRIBA] = juego.getCelda(this.posicion.x,this.posicion.y - 1);
+		celdasAlrededor[ABAJO] = juego.getCelda(this.posicion.x,this.posicion.y + 1);
+		celdasAlrededor[IZQUIERDA] = juego.getCelda(this.posicion.x,this.posicion.y - 1);
+		celdasAlrededor[DERECHA] = juego.getCelda(this.posicion.x,this.posicion.y + 1);
+		
+		//Descarto moverme hacia atrás
+		celdasAlrededor[direccionContraria(this.direccion)]= null;
+		
+		for (int i = 0; i<4; i++){
+			if(celdasAlrededor[i] != null)
+				if(celdasAlrededor[i].esTransitable())
+					if (objetivo.getDistancia(celdasAlrededor[i].getPosicion()) > distanciaMaxima){
+						distanciaMaxima =objetivo.getDistancia(celdasAlrededor[i].getPosicion());
+						direccionAMaximaDistancia = i;
+					}
+		}
+		
+		if (direccionAMaximaDistancia != -1){
+			return direccionAMaximaDistancia; //Encontró una dirección nueva
+		}
+		return direccionContraria(this.direccion); //Encontró una pared y debe dar vuelta
 	}
 	
 	protected abstract int determinarSiguienteDireccion();
