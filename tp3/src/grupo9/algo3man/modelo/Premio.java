d67b7076655534219cf45bf6ae7958f6b12c15d7
cripton <cripton@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-11-24 12:59:49 +0000

    Agregadas las clases que faltaban, Premio y Teletransportador, que extienden a celda

---
+++ tp3/src/grupo9/algo3man/modelo/Premio.java
@@ -0,0 +1,15 @@
+package grupo9.algo3man.modelo;
+
+public class Premio extends Celda {
+
+	public Premio() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean esTransitable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ tp3/src/grupo9/algo3man/modelo/TeleTransportador.java
@@ -0,0 +1,15 @@
+package grupo9.algo3man.modelo;
+
+public class TeleTransportador extends Celda {
+
+	public TeleTransportador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean esTransitable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
