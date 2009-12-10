7ba1c51425710cb9c849a3ec5fcbae81cbc77d02
macaco@gmail.com <macaco@gmail.com@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-12-10 05:50:07 +0000

    Dibujar fantasmas de distinto color segun su estado

--- tp3/src/grupo9/algo3man/vista/VistaFantasmaNaranja.java
+++ tp3/src/grupo9/algo3man/vista/VistaFantasmaNaranja.java
@@ -1,19 +1,29 @@
 package grupo9.algo3man.vista;
 
+import java.awt.Color;
+
 import grupo9.algo3man.modelo.Fantasma;
 import grupo9.algo3man.titiritero.Dibujable;
 import grupo9.algo3man.titiritero.Posicionable;
 import grupo9.algo3man.titiritero.SuperficieDeDibujo;
+import grupo9.algo3man.titiritero.vista.Circulo;
 
-public class VistaFantasmaNaranja implements Dibujable {
+public class VistaFantasmaNaranja extends Circulo implements Dibujable {
 	private Fantasma fantasma;
+	final private static int RADIO = 3;
 	
 	public VistaFantasmaNaranja(Fantasma fantasma){
+		super(RADIO);
 		this.fantasma = fantasma;
 	}
 
 	public void dibujar(SuperficieDeDibujo superfice) {
-		// TODO Auto-generated method stub
+		if(this.fantasma.getEstado() == Fantasma.HUYENDO)
+			this.setColor(Color.ORANGE);
+		else
+			this.setColor(Color.BLUE);
+		
+		//TODO definir posicion
 		
 	}
 
