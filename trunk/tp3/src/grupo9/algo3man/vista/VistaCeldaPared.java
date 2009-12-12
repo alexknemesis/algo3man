42e7a4b593c62aa4f489b676470979db6869a08b
macaco@gmail.com <macaco@gmail.com@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-12-12 21:07:12 +0000

    Correccion en Punto Posicionable

--- trunk/tp3/src/grupo9/algo3man/vista/VistaCeldaPared.java
+++ trunk/tp3/src/grupo9/algo3man/vista/VistaCeldaPared.java
@@ -3,7 +3,6 @@ package grupo9.algo3man.vista;
 import java.awt.Color;
 
 import grupo9.algo3man.modelo.CeldaPared;
-import grupo9.algo3man.modelo.CeldaPunto;
 import grupo9.algo3man.titiritero.Dibujable;
 import grupo9.algo3man.titiritero.Posicionable;
 import grupo9.algo3man.titiritero.SuperficieDeDibujo;
