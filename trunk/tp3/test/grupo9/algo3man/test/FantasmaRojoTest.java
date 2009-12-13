1dedbd04dc3e965394a5f23312eaf8b64c3fc17b
transparencia.tornasolada <transparencia.tornasolada@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-12-13 14:25:26 +0000

    Depurando el modelo: FantasmaRojoTest listo. Up next FantasmaVioletaTest

--- trunk/tp3/test/grupo9/algo3man/test/FantasmaRojoTest.java
+++ trunk/tp3/test/grupo9/algo3man/test/FantasmaRojoTest.java
@@ -5,48 +5,115 @@ package grupo9.algo3man.test;
 
 import java.awt.Point;
 
-import grupo9.algo3man.modelo.CeldaPared;
 import grupo9.algo3man.modelo.Fantasma;
 import grupo9.algo3man.modelo.FantasmaRojo;
 import grupo9.algo3man.modelo.Juego;
-import grupo9.algo3man.modelo.Mapa;
+import grupo9.algo3man.modelo.Pacman;
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 /**
- * @author cripton
+ * @author Lucas Robles
  *
  */
 public class FantasmaRojoTest extends TestCase {
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
 	private Juego juego;
-	private Fantasma fantasma;
+	private FantasmaRojo fantasma;
+	private Pacman pacman;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		
 		this.juego = new Juego();
-		this.fantasma = this.juego.getFantasmas().get(0);
-	}
-
-	/**
-	 * Test method for {@link grupo9.algo3man.modelo.FantasmaRojo#determinarSiguienteDireccion()}.
-	 */
-	public void testConstructor(){
-		assertNotNull(fantasma);
+		this.fantasma = (FantasmaRojo) this.juego.getFantasmas().get(1);
+		this.pacman = juego.getPacman();
 	}
 	
-	public void testDeterminarSiguienteDireccion() {
-		fail("Not yet implemented");
+	public void testMorir() {
+		this.fantasma.morir();
+		assertEquals(this.fantasma.getPosicionInicial(), this.fantasma.getPosicion());
+		assertEquals(Fantasma.DISPERSO, fantasma.getEstado());
+		
 	}
 
-	/**
-	 * Test method for {@link grupo9.algo3man.modelo.Fantasma#vivir()}.
-	 */
-	public void testVivir() {
-		fail("Not yet implemented");
+	public void testEstaEnModoDisperso(){
+		Assert.assertEquals(Fantasma.DISPERSO, this.fantasma.getEstado());
+	}
+	
+	public void testEstaEnModoCazando(){
+		for (int i=0; i < 20+1; i++){ // 20 son los tics para salir del modo disperso
+			this.fantasma.vivir();
+		}
+		Assert.assertEquals(Fantasma.CAZANDO, this.fantasma.getEstado());
+	}
+	
+	public void testEstaEnModoHuyendo(){
+		this.pacman.setPosicion(new Point(2,3));
+		this.pacman.vivir();
+		this.fantasma.vivir();
+		assertEquals(Fantasma.HUYENDO, this.fantasma.getEstado());
+	
+	}
+	
+	public void testEstabaEnModoCazandoYVuelveAlEstadoDisperso(){
+		this.pacman.setPosicion(new Point(14,6)); //posición imposible de llegar
+		
+		for (int i=0; i < 20+1; i++){ // 20 son los tics para salir del modo disperso
+			this.fantasma.vivir();
+		}
+		for (int i=0; i < 20+1; i++){ // 20 son los tics para volver a disperso
+			this.fantasma.vivir();
+		}
+		Assert.assertEquals(Fantasma.DISPERSO, this.fantasma.getEstado());
+		
+	}
+	
+	public void testEstabaHuyendoYVuelveADisperso(){
+		this.pacman.setPosicion(new Point(2,3));
+		this.pacman.vivir();
+		this.fantasma.vivir();
+		for (int i=0; i < 10; i++){ 
+			this.pacman.vivir();
+		}
+		this.fantasma.vivir();
+		Assert.assertEquals(Fantasma.DISPERSO, this.fantasma.getEstado());
+		
+	}
+	
+	public void testEstaEnModoDispersoYSeDirijeASuPosicionPreferida(){
+		for (int i=0; i < 10; i++){ 
+			//System.out.println(this.fantasma.getPosicion());
+			this.fantasma.vivir();
+		}
+		Assert.assertEquals(new Point(12,11), this.fantasma.getPosicion());
+		
+	}
+	
+	public void testEstaEnModoDispersoSeComeAlPacmanYComeVida(){
+		this.pacman.setPosicion(new Point(12,7));
+		for (int i=0; i < 6; i++){ 
+			this.fantasma.vivir();
+		}
+		//System.out.println(this.fantasma.getPosicion());
+		
+		
+		Assert.assertEquals(2, this.juego.getVidas());
+		Assert.assertEquals(this.fantasma.getPosicionInicial(), this.fantasma.getPosicion());
+	}
+	
+	public void testEstaEnModoCazandoSeComeAlPacmanYComeVida(){
+		this.pacman.setPosicion(new Point(16,9));
+		for (int i=0; i < 20+1; i++){ 
+			this.fantasma.vivir();
+		}
+		System.out.println(this.fantasma.getPosicion());
+		for (int i=0; i < 5; i++){ 
+			this.fantasma.vivir();
+		}
+		System.out.println(this.fantasma.getPosicion());
+		Assert.assertEquals(2, this.juego.getVidas());
+		Assert.assertEquals(this.fantasma.getPosicionInicial(), this.fantasma.getPosicion());
 	}
 
 
