85488d04264a255a051a9b85c3ae6f625e22980e
cripton <cripton@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-12-15 20:10:06 +0000

    Agregadas las vistas de los fantasmas copadas, ahora son fantasmas y no puntitos que se mueven :D

--- tp3/src/grupo9/algo3man/vista/VistaFantasmaCeleste.java
+++ tp3/src/grupo9/algo3man/vista/VistaFantasmaCeleste.java
@@ -1,30 +1,46 @@
 package grupo9.algo3man.vista;
 
-import java.awt.Color;
-
 import grupo9.algo3man.modelo.Fantasma;
+import grupo9.algo3man.modelo.Personaje;
 import grupo9.algo3man.vista.PuntoPosicionable;
 import grupo9.algo3man.titiritero.Dibujable;
 import grupo9.algo3man.titiritero.Posicionable;
 import grupo9.algo3man.titiritero.SuperficieDeDibujo;
-import grupo9.algo3man.titiritero.vista.Circulo;
+import grupo9.algo3man.titiritero.vista.Imagen;
 
-public class VistaFantasmaCeleste extends Circulo implements Dibujable {
+public class VistaFantasmaCeleste extends Imagen implements Dibujable {
 	private Fantasma fantasma;
-	
-	final private static int RADIO = 30;
-	
+
 	public VistaFantasmaCeleste(Fantasma fantasma){
-		super(RADIO);
+		super();
 		this.setPosicionable(fantasma);
+
+		this.fantasma = fantasma;
+		this.setNombreArchivoImagen("imagenes/FantasmaCelesteAbajo.png");
 	}
 
 	public void dibujar(SuperficieDeDibujo superfice) {
-		if(this.fantasma.getEstado() != Fantasma.HUYENDO)
-			this.setColor(Color.CYAN);
-		else
-			this.setColor(Color.BLUE);
-		
+
+		if(this.fantasma.getEstado() != Fantasma.HUYENDO){
+
+			switch (this.fantasma.getDireccion()){
+			case Personaje.ABAJO:
+				this.setNombreArchivoImagen("imagenes/FantasmaCelesteAbajo.png");
+				break;
+			case Personaje.ARRIBA:
+				this.setNombreArchivoImagen("imagenes/FantasmaCelesteArriba.png");
+				break;
+			case Personaje.IZQUIERDA:
+				this.setNombreArchivoImagen("imagenes/FantasmaCelesteIzquierda.png");
+				break;
+			case Personaje.DERECHA:
+				this.setNombreArchivoImagen("imagenes/FantasmaCelesteDerecha.png");
+				break;
+			}
+
+		}else
+			this.setNombreArchivoImagen("imagenes/FantasmaAsustado.png");
+		this.setPosicionable(fantasma);
 		super.dibujar(superfice);
 	}
 
@@ -33,6 +49,7 @@ public class VistaFantasmaCeleste extends Circulo implements Dibujable {
 	}
 
 	public void setPosicionable(Posicionable posicionable) {
+
 		this.fantasma = (Fantasma) posicionable;
 	}
 }
--- tp3/src/grupo9/algo3man/vista/VistaFantasmaNaranja.java
+++ tp3/src/grupo9/algo3man/vista/VistaFantasmaNaranja.java
@@ -1,29 +1,46 @@
 package grupo9.algo3man.vista;
 
-import java.awt.Color;
-
 import grupo9.algo3man.modelo.Fantasma;
+import grupo9.algo3man.modelo.Personaje;
 import grupo9.algo3man.vista.PuntoPosicionable;
 import grupo9.algo3man.titiritero.Dibujable;
 import grupo9.algo3man.titiritero.Posicionable;
 import grupo9.algo3man.titiritero.SuperficieDeDibujo;
-import grupo9.algo3man.titiritero.vista.Circulo;
+import grupo9.algo3man.titiritero.vista.Imagen;
 
-public class VistaFantasmaNaranja extends Circulo implements Dibujable {
+public class VistaFantasmaNaranja extends Imagen implements Dibujable {
 	private Fantasma fantasma;
-	final private static int RADIO = 30;
-	
+
 	public VistaFantasmaNaranja(Fantasma fantasma){
-		super(RADIO);
+		super();
 		this.setPosicionable(fantasma);
+
+		this.fantasma = fantasma;
+		this.setNombreArchivoImagen("imagenes/FantasmaNaranjaAbajo.png");
 	}
 
 	public void dibujar(SuperficieDeDibujo superfice) {
-		if(this.fantasma.getEstado() != Fantasma.HUYENDO)
-			this.setColor(Color.GREEN);
-		else
-			this.setColor(Color.BLUE);
-		
+
+		if(this.fantasma.getEstado() != Fantasma.HUYENDO){
+
+			switch (this.fantasma.getDireccion()){
+			case Personaje.ABAJO:
+				this.setNombreArchivoImagen("imagenes/FantasmaNaranjaAbajo.png");
+				break;
+			case Personaje.ARRIBA:
+				this.setNombreArchivoImagen("imagenes/FantasmaNaranjaArriba.png");
+				break;
+			case Personaje.IZQUIERDA:
+				this.setNombreArchivoImagen("imagenes/FantasmaNaranjaIzquierda.png");
+				break;
+			case Personaje.DERECHA:
+				this.setNombreArchivoImagen("imagenes/FantasmaNaranjaDerecha.png");
+				break;
+			}
+
+		}else
+			this.setNombreArchivoImagen("imagenes/FantasmaAsustado.png");
+		this.setPosicionable(fantasma);
 		super.dibujar(superfice);
 	}
 
@@ -32,6 +49,7 @@ public class VistaFantasmaNaranja extends Circulo implements Dibujable {
 	}
 
 	public void setPosicionable(Posicionable posicionable) {
+
 		this.fantasma = (Fantasma) posicionable;
 	}
 }
