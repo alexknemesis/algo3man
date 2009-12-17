4ff70c042d19871122185d852a1288cc76f852a3
cripton <cripton@7409ece4-cfd3-11de-bdd5-43b5fbae6bf9>
2009-12-17 04:25:21 +0000

    Agregado el título a la aplicación

--- trunk/tp3/src/grupo9/algo3man/vista/Algo3Man.java
+++ trunk/tp3/src/grupo9/algo3man/vista/Algo3Man.java
@@ -6,6 +6,7 @@ import java.awt.MenuItem;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import grupo9.algo3man.controlador.EscuchadorDeTeclado;
 import grupo9.algo3man.modelo.Juego;
 import grupo9.algo3man.vista.PuntoPosicionable;
@@ -41,6 +42,7 @@ public class Algo3Man extends Ventana implements ActionListener{
 		MenuBar barraMenu = new MenuBar();
 		barraMenu.add(menu);
 		this.setMenuBar(barraMenu);
+		this.setTitle("Algo3Man");
 		
 		 
 		
@@ -56,7 +58,7 @@ public class Algo3Man extends Ventana implements ActionListener{
 		int dimensionX = (int) (juego.getDimensiones().getX()*posicionable.getX());
 		int dimensionY = (int) (juego.getDimensiones().getY()*posicionable.getY());
 		final Algo3Man algo3man = new Algo3Man(dimensionX, dimensionY, juego);
-
+		
 		algo3man.setSize(600,600);
 		algo3man.setResizable(false);
 		algo3man.setVisible(true);
@@ -72,6 +74,12 @@ public class Algo3Man extends Ventana implements ActionListener{
 		if(e.getSource() == this.menuItemStart){
 			sonidoComienzo = new SonidoThread("sonidos/pac_start.wav");
 			sonidoComienzo.start();
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			this.juego.comenzarJuego();
 			this.menuItemPause.setEnabled(true);
 			
